75e1f9c83a3b6b754f4276382b008f7626da2fd5
Subhansh garg <subhanshgarg2965@gmail.com>
2024-09-07 16:06:17 +0530

    Add .gitignore file

--- product-service/src/main/java/com/product_service/product_service/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.product_service.product_service.Repository;
-
-import com.product_service.product_service.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
-}
